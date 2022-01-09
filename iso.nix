ba704d49a1bbc68c687c6fce7a3a2f94ffae1c3c
Vanilla <osu_Vanilla@126.com>
2022-01-09 21:33:14 +0800

    [+] iso.nix: Build 21.11 minimal iso with firmwareLinuxNonfree.

---
+++ iso.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+  ];
+
+  hardware.firmware = with pkgs;
+    [ firmwareLinuxNonfree ];
+}
