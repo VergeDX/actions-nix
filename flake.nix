1a5abec3b605ee5ac0e34d596bfb4397148b7da3
Vanilla <osu_Vanilla@126.com>
2022-01-25 18:03:28 +0800

    [M]: minimal -> latest, update iso.nix.

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixos.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixos, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: rec {
+      pkgs = import nixos { inherit system; };
+      packages."iso" = (nixos.lib.nixosSystem {
+        inherit system;
+
+        modules = [ "${nixos}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix" ]
+          ++ [ "${nixos}/nixos/modules/installer/cd-dvd/channel.nix" ]
+          ++ [{ boot.kernelPackages = pkgs.linuxPackages_latest; }] ++
+          [{ hardware.firmware = with pkgs; [ firmwareLinuxNonfree ]; }];
+      }).config.system.build.isoImage;
+    });
+}
